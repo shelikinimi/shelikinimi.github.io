4886f9bdb74096b5525746307daffe1adaf20a87
shelikinimi <140999904+shelikinimi@users.noreply.github.com>
2023-08-06 15:47:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function show() {
+  document
+    .getElementById("contactForm")
+    .setAttribute("style", "width:100%;height:100%");
+}
+
+function hide() {
+  document
+    .getElementById("contactForm")
+    .setAttribute("style", "width:0;height:0");
+}
